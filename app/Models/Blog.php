c739817e67178bf0bcb4eb70b6c93e33e2c39e2a
JawadHardley <jawadcharls@gmail.com>
2024-07-13 23:25:05 +0300

    Dynamic run 3

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,35 +2,9 @@
 
 namespace App\Models;
 
-class Blog
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 0,
-                'title' => 'Blog Post 1',
-                'body' => 'This is the body of the blog post 1',
-            ],
-            [
-                'id' => 1,
-                'title' => 'Blog Post 2',
-                'body' => 'This is the body of the blog post 2',
-            ]
-        ];
-    }
-
-    public static function findBlog($id)
-    {
-        $blogId = [];
-        foreach (static::all() as $g) {
-            $blogId[] = $g['id'];
-        }
-
-        if (in_array($id, $blogId)) {
-            return true;
-        } else {
-            abort(404);
-        }
-    }
+    //wait
 }
