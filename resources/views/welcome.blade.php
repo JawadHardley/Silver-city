03745505a0ceea8a5a7bcac4998df66c11d26367
JawadHardley <jawadcharls@gmail.com>
2024-07-15 23:51:48 +0300

    Design round 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             @foreach($blog as $bog) 
 
-                <x-nav-link href="/blog/{{$bog['id']}}">
+                <a href="/blog/{{$bog['id']}}">
                     <div class="max-w-sm rounded overflow-hidden shadow-lg -mx-2">
                         <img class="w-full"
                             src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -17,7 +17,7 @@
                             <div class="font-bold text-xl mb-2">{{$bog['title']}}</div>
                         </div>
                     </div>
-                </x-nav-link>
+                </a>
 
             @endforeach
         </div>
