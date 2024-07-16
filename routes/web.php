ab0a77607c2c3435b37e7d248b9e4fb765102de4
JawadHardley <jawadcharls@gmail.com>
2024-07-16 19:21:54 +0300

    Eager loading 7

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Models\Job;
 
 Route::get('/', function () {
     return view('welcome', [
-        'blog' => Job::with('employer')->paginate(6),
+        'blog' => Job::with('employer')->paginate(3),
     ]);
 });
 
