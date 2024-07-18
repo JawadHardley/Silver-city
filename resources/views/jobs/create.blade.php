6c91fed48ff9ef9e8f18519b3de2ad5591bcb38c
JawadHardley <jawadcharls@gmail.com>
2024-07-18 21:23:06 +0300

    Form 11

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -36,6 +36,15 @@
                         </div>
                     </div>
                 </div>
+                <div class="mt-10">
+                    @if($errors->any())
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
             </div>
         </div>
 
